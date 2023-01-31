cafc56d30bc63e042ba4ba3ee4a880a5c8c7ab1f
Jacob David Diehl <jakediehl17@gmail.com>
2023-01-31 14:26:55 -0700

    commit tree acfe2c4d65f7a09e2bbd2a4def48a9785ce51104

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,39 @@
 # your code goes here
+class Person
+    attr_reader :name, :hygiene, :happiness
+    attr_accessor :bank_account
+
+    def initialize (name, bank_account=25, happiness=8, hygiene=8)
+        @name=name
+        @bank_account=bank_account
+        @happiness=happiness
+        @hygiene=hygiene
+    end
+
+
+    def change_happiness(num)
+       @happiness = if num <= 10
+        @happiness = num
+        else puts "happiness can't exceed 10"
+        end
+    end
+
+    def change_hygiene(hygiene)
+        if hygiene <= 10 
+            self.hygiene = hygiene
+        else puts "hygiene can't exceed 10"
+        end
+    end
+
+end
+
+
+jake = Person.new(name:'Jake')
+# puts jake
+puts jake.name
+puts jake.happiness
+# puts jake.bank_account
+# puts jake.hygiene
+
+puts jake.change_happiness(9)
+# puts jake.hygiene=10
